3aa6c1bc1dadfd994b956a5386ad7499cd71c441
m-ux-k <mohammed3002m@gmail.com>
2026-09-23 14:32:36 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -13,11 +13,14 @@
  * -------------------------------------------------------------
  */
 
-const FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
+const firebaseConfig = {
+  apiKey: "AIzaSyAKegHJLHEdqjRIIMriU_6l4Hbp2B23yBE",
+  authDomain: "schedule-7ac07.firebaseapp.com",
+  projectId: "schedule-7ac07",
+  storageBucket: "schedule-7ac07.firebasestorage.app",
+  messagingSenderId: "282137992280",
+  appId: "1:282137992280:web:12f6f95fb8a98c7317be2d",
+  measurementId: "G-WNS8JS5FZS"
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID",
 };
